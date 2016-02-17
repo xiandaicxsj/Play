1aff96b683328ca08d94c3252fad38f5e83a62d7
joker <joker@jk>
2016-02-17 14:58:59 +0800

    loader now work
    head_32.s paging is not testing
    
    Signed-off-by: joker <joker@jk>

--- kernel.lds.h
+++ kernel.lds.h
@@ -1,6 +1,6 @@
 #ifndef _KERNE_LDS_H
 #define _KERNE_LDS_H
-#define KERNEL_ADDR 0xc000000
+#define KERNEL_ADDR 0xc0000000
 #define LOAD_ADDR 0x9c00
 
 #endif
---
+++ vmlds.h
@@ -0,0 +1,8 @@
+#ifndef _H_VMLDS
+#define _H_VMLDS
+extern char _bss_end[];
+extern char _bss_start[];
+extern char _text_start[];
+extern char _text_end[];
+
+#endif

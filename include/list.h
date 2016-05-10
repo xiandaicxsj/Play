115e9bc075972bd48e3a33ad2c4251737d202dd8
joker <joker@jk>
2016-05-10 22:17:01 +0800

    kernel: add list support
    Signed-off-by: joker <joker@jk>

---
+++ include/list.h
@@ -0,0 +1,17 @@
+#ifndef _H_LIST
+#define _H_LIST
+struct list_head
+{
+	struct list_head *next;
+	struct list_head *next;
+}
+static void list_add(struct list_head *prev, struct list_head *next)
+{
+}
+
+static void list_del(struct list_head *prev, struct list_head *next)
+{
+
+}
+
+#endif

16aa81a89ff5ca062f082e34c41fff1a01d9a00a
joker <joker@jk>
2016-05-12 17:32:38 +0800

    fix list bug
    Signed-off-by: joker <joker@jk>

--- include/list.h
+++ include/list.h
@@ -4,10 +4,10 @@ struct list_head
 {
 	struct list_head *next;
 	struct list_head *prev;
-}
+};
 
 /* double rotate list */
-static inline void _list_add(struct list_head *prev, struct list_head *new, struct list_head *next)
+inline void _list_add(struct list_head *prev, struct list_head *new, struct list_head *next)
 {
 	next->prev= new;
 	new->next = next;
@@ -15,30 +15,32 @@ static inline void _list_add(struct list_head *prev, struct list_head *new, stru
 	new->prev = prev;
 
 }
-static inline void list_add(struct list_head *new, struct list_head *head)
+
+inline void list_add(struct list_head *new, struct list_head *head)
 {
 	_list_add(head->prev, new, head);
 }
 
-static inline void _list_del(struct list_head *prev, struct list_head *next)
+inline void _list_del(struct list_head *prev, struct list_head *next)
 {
 	prev->next = next;
 	next->prev = prev;
 }
 
-static inline void init_list(struct list_head *new)
+inline void init_list(struct list_head *new)
 {
 	new->next = new;
 	new->prev = new;
 }
+
 static inline void list_del(struct list_head *entry)
 {
 	_list_del(entry->prev, entry->next);
 }
 
 /* member -> type */
-#define offsetof(type, member) (((type *)0)->member)
+#define offsetof(type, member) ((size_t) &((type *)0)->member)
 #define container_of(ptr, type, member) ({ \
 	const typeof( ((type *)0)->member) * _mptr = (ptr); \
-	(type *)( (char *)_mptr - offsetof(type, member))})
+	(type *)( (char *)_mptr - offsetof(type, member));})
 #endif

630ef965141175dd844f0108d4dd1f9de25d7054
joker <joker@jk>
2016-05-12 18:32:08 +0800

    math: add _log
    Signed-off-by: joker <joker@jk>

--- include/math.h
+++ include/math.h
@@ -2,4 +2,18 @@
 #define _H_MATH
 #include"type.h"
 u32 round_up(u32 base, u32 align);
+u32 inline _log(u32 num)
+{
+	u32 count = 0;
+	int tmp =1;
+	while(1)
+	{
+		if(num <= tmp)
+			break;
+		tmp = tmp << 1;
+		count ++;
+	}
+        return count;
+}
+
 #endif

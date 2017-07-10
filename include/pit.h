55f86cd5b639be72d0569e50104c2e974a48433f
joker <joker@jk.com>
2017-07-10 14:59:01 +0800

    Add pit logic
    Signed-off-by: joker <joker@jk.com>

---
+++ include/pit.h
@@ -0,0 +1,28 @@
+#ifndef _H_PIT
+#define _H_PIT
+
+#define PIT_CH0_PORT  0x40
+#define PIT_CH1_PORT  0x41
+#define PIT_CH2_PORT  0x42
+
+#define PIT_MODE_PORT 0x43 
+
+#define PIT_CH0 0
+#define PIT_CH1 1
+#define PIT_CH2	2
+#define PIT_RD  3
+
+#define PIT_OPM_IOTC 0
+#define PIT_OPM_HRTO 1
+#define PIT_OPM_RATE 2
+#define PIT_OPM_SQ_WAVE 3
+#define PIT_OPM_SF_STROBE 4
+#define PIT_OPM_HD_STROBE 5
+#define PIT_OPM_RATE2 6
+#define PIT_OPM_SQ_WAVE2 7
+
+#define PIT_ACM_LO 1
+#define PIT_ACM_HO 2
+#define PIT_ACM_LH 3
+
+#endif
---
+++ kernel/pit.c
@@ -0,0 +1,46 @@
+#include"pit.h"
+#include"type.h"
+#include"port.h"
+
+static void pit_set_chanel_mode(u8 ch, u8 opera_mode, u8 access_mode)
+{
+	u8 data = 0;
+	
+	data =  (ch << 6) | (opera_mode  << 1) | (access_mode << 4);
+	outb(PIT_MODE_PORT, data);
+
+}
+static void pit_set_reload_value(u8 ch, u16 value)
+{
+	u8 data;
+
+	data = value & 0xff;
+	outb(PIT_CH0_PORT + ch, data); 
+
+	data = value >> 8;
+	outb(PIT_CH0_PORT + ch, data); 
+}
+
+static get_timer_interval(void)
+{
+	return 0;
+}
+
+static u16 pit_cal_reload_value(u16 interval)
+{
+	return 2000;
+}
+
+void init_pit(void) {
+	u16 value;
+
+	local_irq_disable();
+
+	value = pit_cal_reload_value(get_timer_interval());
+
+	pit_set_chanel_mode(PIT_CH0, PIT_OPM_RATE, PIT_ACM_LH);
+	pit_set_reload_value(PIT_CH0, value);
+
+	local_irq_enable();
+	return;
+}

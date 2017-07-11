a6b6d57c3fa2f9436166260b17f36c1088bd6e84
joker <joker@jk.com>
2017-07-11 14:05:57 +0800

    First timer refractor pic code
    Signed-off-by: joker <joker@jk.com>

--- include/pic.h
+++ include/pic.h
@@ -1,11 +1,19 @@
 #ifndef _H_PIC
 #define _H_PIC
 #include"type.h"
-#define PIC_MASTER 0x20
-#define PIC_SLAVE 0x21
+#define PIC_MASTER 1
+#define PIC_SLAVER 0
+
+#define PIC_MASTER_CMD 0x20
+#define PIC_MASTER_DATA 0x21
+#define PIC_SLAVER_CMD 0xA0
+#define PIC_SLAVER_DATA 0xA1
+#define MS_IRQ_BASE 0x20
+#define SL_IRQ_BASE 0x28
+
+#define PIC_EOI 0x20
 #define ICW1  0x11
-#define MS_INTER_BASE 0x20
-#define SL_INTER_BASE 0x28
+
 
 
 void init_pic();
--- kernel/pic.c
+++ kernel/pic.c
@@ -2,42 +2,55 @@
 #include"type.h"
 #include"port.h"
 
-static void _init_pic(u8 port, u8 inter_offset, u32 is_master)
+static void _init_pic(u8 is_master, u8 irq_vec_off)
 {
-	outb(port, 0x11); /* icw1 */
-	outb(port + 1, inter_offset);  /* icw2 */
+	u8 cmd_port = is_master ? PIC_MASTER_CMD : PIC_SLAVER_CMD;
+	u8 data_port = is_master ? PIC_MASTER_DATA : PIC_SLAVER_DATA;
+
+	outb(cmd_port, 0x11); /* icw1 */
+	outb(data_port, irq_vec_off);  /* icw2 */
+
 	if (is_master)
-		outb(port + 1, 0x40); /*icw3 */
+		outb(data_port, 0x40); /*icw3 */
 	else 
-		outb(port + 1, 0x20);
-	outb(port + 1, 0x01); /* icw4 */
+		outb(data_port, 0x20);
+	outb(data_port, 0x01); /* icw4 */
 }
 
-static void _mask_interrupt(u8 port, u8 mask)
+static void pic_set_irqmask(u8 is_master, u8 mask)
 {
-	outb(port + 1, mask); 	
+	u8 data_port;
+
+       	data_port = is_master ? PIC_MASTER_DATA : PIC_SLAVER_DATA;
+	outb(data_port, mask);
 }
 
-void mask_interrupt(u32 index)
+void pic_mask_irq(u32 vector)
 {
-	u8 mask = 0;
-	u8 port = index < SL_INTER_BASE ? PIC_MASTER:PIC_SLAVE;
-	u8 pic_off = index < SL_INTER_BASE ? index - MS_INTER_BASE :
-		  				 index - SL_INTER_BASE;
-
-	mask = inb(port + 1);
-	mask &= 0 << pic_off;
-	_mask_interrupt(PIC_MASTER, mask);
+	u8 is_master;
+	u8 mask;
+	u8 data_port;
+	
+	is_master = vector < SL_IRQ_BASE ? PIC_MASTER : PIC_SLAVER;
+       	data_port = is_master ? PIC_MASTER_DATA : PIC_SLAVER_DATA;
+	mask = inb(data_port);
+	
+	mask |= 1 << (is_master ? vector - MS_IRQ_BASE : vector - SL_IRQ_BASE);
+	pic_set_irqmask(is_master, mask);
 }
 
 void send_eoi(u32 vector)
 {
-	outb(vector, PIC_MASTER);
+	u8 cmd_port;
+
+	cmd_port = vector < SL_IRQ_BASE ? PIC_MASTER_DATA : PIC_SLAVER_DATA;
+	outb(cmd_port, PIC_EOI);
 }
 
 void init_pic()
 {
-	_init_pic(PIC_MASTER, MS_INTER_BASE, 1);
-	_init_pic(PIC_SLAVE, SL_INTER_BASE, 0);
-	_mask_interrupt(PIC_MASTER, 0x0);
+	_init_pic(PIC_MASTER, MS_IRQ_BASE);
+	_init_pic(PIC_SLAVER, SL_IRQ_BASE);
+	pic_set_irqmask(PIC_MASTER, 0x0);
+	pic_set_irqmask(PIC_SLAVER, 0x0);
 }

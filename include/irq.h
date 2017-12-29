7271a50a45ca39b99871711da5baa93c166ca872
joker <joker@jk.com>
2017-12-29 15:52:17 +0800

    introduce irqsave/restore();
    Signed-off-by: joker <joker@jk.com>

--- include/irq.h
+++ include/irq.h
@@ -11,4 +11,22 @@ static inline void local_irq_enable()
 	asm volatile ( "sti" );
 }
 
+static inline void local_irq_save(u32 *rf) 
+{
+	u32 flags;
+	asm volatile ("pushfl\n\t"
+		      "popl %0\n\t"
+		      :"=rm"(flags)::"memory");
+	*rf = flags;
+	local_irq_disable();
+}
+
+static inline void local_irq_restore(u32 *rf)
+{
+	u32 flags;
+	flags = *rf;
+	asm volatile ("pushl %%eax\n\t"
+		      "popfl\n\t"
+		      ::"a"(flags):);
+}
 #endif

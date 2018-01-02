9a115433e22f15020ce7a35603c25baa5f12e668
joker <joker.jk.com>
2018-01-02 13:32:57 -0600

    Add irq_chip abstract layer
    Signed-off-by: joker <joker.jk.com>

---
+++ include/irq_chip.h
@@ -0,0 +1,17 @@
+#ifndef _H_IRQCHIP
+#define _H_IRQCHIP
+#include"types.h"
+
+struct irq_ops {
+	int (*) 
+	u32 (*hwirq_to_irq)(u32 hwirq);
+	u32 (*mask_irq)(u32 vector);
+
+};
+
+struct irq_chip {
+	struct irq_ops *ops;
+	void *owner;
+};
+
+#endif
